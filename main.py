54087ef63064038535d5096edfb3937c39fce784
saturdaymofever <saturdaymofever@gmail.com>
2025-09-13 09:59:54 +0200

    test

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sys, pygame
 pygame.init()
 pygame.font.init()
-size = width, height = 1920,1080
-
-black = 0, 0, 0
-
 my_font = pygame.font.SysFont('Comic Sans MS', 100)
 text_surface = my_font.render('Dungeon Term "PyGame Edition"', False, (255, 255, 255))
+size = width, height = 1920,1080
+text_rect = text_surface.get_rect(center=(width // 2, height // 2))
+black = 0, 0, 0
+
+
+
 screen = pygame.display.set_mode(size)
 
 
@@ -15,5 +17,5 @@ while True:
         if event.type == pygame.QUIT: sys.exit()
     
     screen.fill(black)
-    screen.blit(text_surface, (0,0))
+    screen.blit(text_surface, text_rect)
     pygame.display.flip()
---
+++ player.py
@@ -0,0 +1,8 @@
+class Player():
+    pv = None
+    pw = None
+    coord = None
+
+    def __init__(self, pv, pw):
+        
+        def self.coord
