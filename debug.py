1658af05ca922b36a9ca1fd5d04bfcc51f7797de
saturdaymofever <saturdaymofever@gmail.com>
2025-09-13 10:55:11 +0200

    ajout des ressource img

--- debug.py
+++ debug.py
@@ -1,6 +1,10 @@
-import player
+import player, monster
 
 player1 = player.Player()
 player1.move()
 
+monster1 = monster.Monster()
+monster1.move()
+
 print(player1.pv, player1.coord)
+print(monster1.pv, monster1.coord)
