beb794ffe9be5d3477826bddefbf83925b4d6095
saturdaymofever <saturdaymofever@gmail.com>
2025-09-20 14:10:51 +0200

    add dictionnary for bg game

---
+++ background.py
@@ -0,0 +1,24 @@
+import pygame, random
+
+background_dictionary = [
+    {
+        "name": "Clairière enchantée", 
+        "image": "/chemin vers image"  
+    },
+    {
+        "name": "Rivière des murmures", 
+        "image": "/chemin vers image"  
+    },
+    {
+        "name": "Arbre ancien", 
+        "image": "/chemin vers image"  
+    },
+    {
+        "name": "Sentier brumeux", 
+        "image": "/chemin vers image"  
+    },
+    {
+        "name": "Ruines moussues", 
+        "image": "/chemin vers image"  
+    }
+]
